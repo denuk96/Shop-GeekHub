c5772f81c10768d54a9c04cd43a28495954832ee
denuk96 <e_den9611@live.com>
2019-12-25 15:51:57 +0200

    images uploader updated (now we can upload one by one without overwriting)

--- app/controllers/backoffice/images_controller.rb
+++ app/controllers/backoffice/images_controller.rb
@@ -1,11 +1,11 @@
 class Backoffice::ImagesController < Backoffice::BackofficeController
   before_action :set_product
 
-  #def create
-  #  add_more_images(images_params[:images]))
-  #  flash[:error] = "Failed uploading images" unless @product.save
-  #  redirect_to :back
-  #end
+  def create
+    add_more_images(images_params[:images])
+    flash[:error] = "Failed uploading images" unless @product.save
+    redirect_to edit_admin_product_path(@product)
+  end
 
   def destroy
     remove_image_at_index(params[:id].to_i)
