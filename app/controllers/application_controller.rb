82422f8b82f0c6b136d52ccecaea5203aa9fe18b
denuk96 <e_den9611@live.com>
2019-12-26 20:30:32 +0200

    user login after logining fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def current_user_exist?
+    redirect_to home_path if current_user.present?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :current_user_exist?, except: :destroy
+
   def new; end
 
   def create
