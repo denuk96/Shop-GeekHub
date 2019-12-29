51f1d4828f420a79c78ce2e5a89b99ab5f20c17e
denuk96 <e_den9611@live.com>
2019-12-29 17:57:00 +0200

    stars for comments added

---
+++ app/javascript/packs/star.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.star').click(function(){
+        $('.star').css('color', 'grey').css("font-size","40px");
+        $(this).css('color', 'white').css('font-size', '50px');
+    });
+});
+
+$(document).ready(function(){
+    $(".star").hover(  //
+        function(){
+            $(this).animate( { fontSize:"1.5em" } , 300 );},
+        function(){
+            $(".star").animate( { fontSize:"1em" } , 200 );}
+    );
+});
