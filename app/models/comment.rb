6dbd2bda4377b17739584dbefd3f77aa20277bd8
denuk96 <e_den9611@live.com>
2019-12-30 18:53:22 +0200

    minor changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,7 +9,7 @@
 #  product_id :bigint           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-# 
+#
 
 class Comment < ApplicationRecord
   belongs_to :user
