75899b0f7256281d7e396e4352c7b335002e18c2
denuk96 <e_den9611@live.com>
2019-12-31 17:40:08 +0200

    animation bug fixed (scripts replaced)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 require('jquery')
 import '../stylesheets/application'
 import './bootstrap_custom.js'
-import './star.js'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
