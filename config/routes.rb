44b50fb53e6a590e4c63bea16d9d30dbb54c5cfb
Denys Taradada <denis.taradada@gmail.com>
2020-01-06 11:33:36 +0200

    locales switch buttons added

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  root 'products#index', as: 'home'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root 'products#index', as: 'home'
 
-  resources :products do
-    resources :images, controller: 'backoffice/images'
-    resources :comments
+    resources :products do
+      resources :images, controller: 'backoffice/images'
+      resources :comments
+    end
+
+    scope :admin do
+      resources :categories, controller: 'backoffice/categories', as: 'admin_categories'
+      resources :products, controller: 'backoffice/products', as: 'admin_products'
+    end
+
+    resources :users, :sessions
+
+    get 'signup', to: 'users#new', as: 'signup'
+    get 'login', to: 'sessions#new', as: 'login'
+    get 'logout', to: 'sessions#destroy', as: 'logout'
   end
-
-  scope :admin do
-    resources :categories, controller: 'backoffice/categories', as: 'admin_categories'
-    resources :products, controller: 'backoffice/products', as: 'admin_products'
-  end
-
-  resources :users, :sessions
-
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
