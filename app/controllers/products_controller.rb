0251f8e0ae8b4b915bfd5ae53f7d8aa45bb31aae
Denys Taradada <denis.taradada@gmail.com>
2020-01-08 12:39:13 +0200

    Styled

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,6 @@ class ProductsController < ApplicationController
                 else
                   Product.all.order(created_at: :desc).paginate(page: params[:page], per_page: 12)
                 end
-    @categories = Category.all
     if params[:category]
       @category = Category.find_by(id: params[:category])
       @products = @category.products.paginate(page: params[:page], per_page: 20)
