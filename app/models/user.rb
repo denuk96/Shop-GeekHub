f7b95ac96f680386d2f3f65a57d296ff7d5f0f04
Denys Taradada <denis.taradada@gmail.com>
2020-01-16 14:07:45 +0200

    ActiveMailer added, welcome messages added after create

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,11 +23,19 @@ class User < ApplicationRecord
   validates :password, presence: true, allow_nil: true
   validate :pass_val
 
+  after_create :send_welcome
+
+  private
+
   def pass_val
     if password.present?
       if password.count('a-z') <= 0 || password.count('A-Z') <= 0 # || password.count('0-9') <= 0
         errors.add(:password, I18n.t('models.user.password_validation'))
       end
     end
+  end
+
+  def send_welcome
+    UserMailer.welcome_email(self).deliver!
   end
 end
