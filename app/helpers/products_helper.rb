8c817a9ec789e4c80f5c291d8ef014f037c69974
Denys Taradada <denis.taradada@gmail.com>
2020-01-19 19:47:19 +0200

    half star added, styled, couple bugs fixed

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,4 +4,22 @@ module ProductsHelper
     total = comments&.sum(:rating)
     @average_rating = total.to_f / comments&.count
   end
+
+  def render_stars(value)
+    output = ''
+    if (1..5).include?(value.floor)
+      value.floor.times { output += '<i class="fas fa-star"></i>' }
+    end
+    if value == (value.floor + 0.5) && value.to_i != 5
+      output += '<i class="fas fa-star-half-alt"></i>'
+      @digit = 1
+    else
+      @digit = 0
+    end
+    empty_star = ((5 - @digit) - value.floor)
+    empty_star.times do
+      output += '<i class="fas fa-star empty-star"></i>'
+    end
+    output.html_safe
+  end
 end
