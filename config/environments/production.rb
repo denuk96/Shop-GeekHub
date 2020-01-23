ab73914f65d5df6b2ac2e9bb1e278400a2de157f
denuk96 <e_den9611@live.com>
2020-01-23 20:38:17 +0200

    try to fix production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
     port: 587,
     domain: 'example.com',
     user_name: 'blog.on.rails.test@gmail.com',
+
     #password: Rails.application.credentials.aws[:gmail],
     password: ENV['GMAIL'],
     authentication: 'plain',
